7f3fd7f44d025db610a45030b28af8c98434fcba
Andrei Aquino <andreimartinbaquino@gmail.com>
2020-01-21 00:42:50 +0800

    Write a function that accepts an integer and prints out all numbers that are both prime and palindrome

---
+++ checkNumberOfWords.js
@@ -0,0 +1,9 @@
+const countWords = str => {
+    console.log(str.split(' ').length);
+}
+
+// countWords('the quick brown fox jumps over the lazy dog');
+//  9
+
+countWords('time after time');
+// 3
